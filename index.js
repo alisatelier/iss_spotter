9f39fe233fded005a528f471e2e055dfa0a85654
alisatelier <194897761+alisatelier@users.noreply.github.com>
2025-07-14 20:36:51 -0600

    Created fetchCoordinatesByIP function

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:', ip);
-});
+//   console.log('It worked! Returned IP:', ip);
+// });
+
+// const myIP = '142.59.180.210';
+
+// fetchCoordsByIP(myIP, (error, data) => {
+//    if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned coordinates:', data);
+// });
--- iss.js
+++ iss.js
@@ -1,23 +1,53 @@
 
 const needle = require('needle');
 
-const fetchMyIP = function(callback) {
-  const url = `https://api.ipify.org?format=json`;
+// const fetchMyIP = function(callback) {
+//   const url = `https://api.ipify.org?format=json`;
 
-  needle.get(url, (error, response, body) => {
+//   needle.get(url, (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
+//     if (response.statusCode !== 200) {
+//       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//       return;
+//     }
+
+//     const ip = response.body.ip;
+//     callback(null, ip);
+//   });
+// };
+
+const fetchCoordsByIP = function(ip, callback) {
+  const urlCoords = `http://ipwho.is/${ip}`;
+
+  needle.get(urlCoords, (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
     }
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
+      callback(Error(`❌ HTTP error: Status Code ${response.statusCode} from ${urlCoords}`), null);
       return;
     }
 
-    const ip = response.body.ip;
-    callback(null, ip);
+    const data = response.body;
+
+    // 3️⃣ API returned success: false (e.g., invalid IP)
+    if (!data.success) {
+      const message = `Success status was false. Server message: ${data.message} for IP ${data.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+
+    const latitude = data.latitude;
+    const longitude = data.longitude;
+    const coords = { latitude, longitude };
+    callback(null, coords);
   });
 };
 
-module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
